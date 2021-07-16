0a174cb2c6f3fd829605e8ab3ee8f1dfbc2546c9
masbay <mzbyoe.prasetya@gmail.com>
2021-07-16 16:49:31 +0800

    sayang

--- js/app.js
+++ js/app.js
@@ -1,5 +1,9 @@
 $(".title").hover(function(){
     $(this).removeClass("shake").addClass("bounceIn");
 });
-var nama = prompt('Hai, Siapa nama lengkapmu?');
-console.log(nama);
+var nama = prompt("Hai, siapa nama lengkapmu?");
+if (nama == "Shinta Aprilia Andriani") {
+    alert("Hai, "+nama+". Izinkan saya, Bayu Wahyu Prasetya Wibowo, meminangmu dengan بِسْمِ ٱللَّٰهِ");
+} else {
+    alert("Hai, "+nama+". Salam Kenal!");
+}
