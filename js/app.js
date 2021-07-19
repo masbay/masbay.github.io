1277748330feb30b95ad31981e13ce950d0d8e6d
masbay <mzbyoe.prasetya@gmail.com>
2021-07-19 23:47:00 +0800

    back to normal

--- js/app.js
+++ js/app.js
@@ -1,9 +1,3 @@
 $(".title").hover(function(){
     $(this).removeClass("shake").addClass("bounceIn");
 });
-var nama = prompt("Hai, siapa nama lengkapmu?");
-if (nama.toUpperCase() == "SHINTA APRILIA ANDRIANI") {
-    alert("Hai, "+nama+". Izinkan saya, Bayu Wahyu Prasetya Wibowo, meminangmu dengan بِسْمِ ٱللَّٰهِ");
-} else {
-    alert("Hai, "+nama+". Salam Kenal!");
-}
